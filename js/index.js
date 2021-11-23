3a32c250061e2dcaf148392036f10cbe1cf4e2f3
Julie Heckler <july.heckler@gmail.com>
2021-11-23 18:31:50 +0100

    createElts instead of cloneNode

--- js/index.js
+++ js/index.js
@@ -1,18 +1,110 @@
 import {recipes} from "./recipes.js";
 console.log(recipes);
 
-/*Get the ingredients*/
-function getIngredient(recipe, index) {
-    for (let j=0; j<recipe.ingredients.length; j++){
-        let newIngredient = document.createElement("li");
-        document.querySelectorAll(".ingredients")[index].appendChild(newIngredient);
-        const recipeIngredient = document.querySelectorAll(".ingredients > li");
-        recipeIngredient[index].innerHTML = recipe.ingredients[j].ingredient;
-    }
+/*** Cards ***/
+/* Create cards */
+function createCard(element){
+    const newDiv = document.createElement("div")
+    return element.appendChild(newDiv).classList.add("card", "p-0", "my-3")
+}
+function createImg(element){
+    const newImg = document.createElement("img")
+    return element.appendChild(newImg).classList.add("w-100")
+}
+function setImg(element){
+    const newSrcImg = element.setAttribute("src", "./images/img.svg")
+    const newAltImg = element.setAttribute("alt", "image plat")
+    return newAltImg, newSrcImg
+}
+function createCardBody(element){
+    const newDiv = document.createElement("div")
+    return element.appendChild(newDiv).classList.add("card-body", "text-left")
+}
+function createNewRowTop(element){
+    const newDiv = document.createElement("div")
+    return element.appendChild(newDiv).classList.add("row", "rowTop", "my-2", "w-100", "flex-nowrap")
+}
+function createNewRecipeName(element){
+    const newDiv = document.createElement("div")
+    return element.appendChild(newDiv).classList.add("recipe", "col-8", "fs-5")
+}
+function createClock(element){
+    const newIClock = document.createElement("i")
+    const newClock = element.appendChild(newIClock)
+    const newClassClock = newClock.classList.add("far", "fa-clock", "iconClock", "col-1", "m-2")
+    return newClassClock
+}
+function setClock(element){
+    const newAriaClock = element.setAttribute("aria-hidden", "true")
+    return newAriaClock
+}
+function createNewTime(element){
+    const newDiv = document.createElement("div")
+    return element.appendChild(newDiv).classList.add("time", "col-3", "fw-bold", "fs-6")
+}
+function createNewRowBottom(element){
+    const newDiv = document.createElement("div")
+    return element.appendChild(newDiv).classList.add("row", "rowBottom", "fs-6")
+}
+function createNewIngredient(element){
+    const newDiv = document.createElement("div")
+    return element.appendChild(newDiv).classList.add("ingredients", "col-5")
+}
+function createNewUl(element){
+    const newUl = document.createElement("ul")
+    return element.appendChild(newUl).classList.add("ulIngredients")
+}
+function createNewLi(element){
+    const newLi = document.createElement("li")
+    return element.appendChild(newLi)
+}
+function createNewDescription(element){
+    const newDiv = document.createElement("div")
+    return element.appendChild(newDiv).classList.add("description", "col-7")
 }
 
+recipes.map(recipe => createCard(document.getElementById("cards")))
+const cards = [...document.querySelectorAll(".card")]
+cards.map(card => createImg(card))
+const images = [...document.querySelectorAll(".card > img")]
+images.map(image => setImg(image))
+cards.map(card => createCardBody(card))
+const cardBodies = [...document.querySelectorAll(".card-body")]
+cardBodies.map(cardBodie => createNewRowTop(cardBodie))
+const topRows = [...document.querySelectorAll(".rowTop")]
+topRows.map(topRow => createNewRecipeName(topRow))
+topRows.map(topRow => createClock(topRow))
+const clockIcons = [...document.querySelectorAll(".iconClock")]
+clockIcons.map(clockIcon => setClock(clockIcon))
+topRows.map(topRow => createNewTime(topRow))
+cardBodies.map(cardBodie => createNewRowBottom(cardBodie))
+const bottomRows = [...document.querySelectorAll(".rowBottom")]
+bottomRows.map(bottomRow => createNewIngredient(bottomRow))
+const ingredients = [...document.querySelectorAll(".ingredients")]
+ingredients.map(ingredient => createNewUl(ingredient))
+bottomRows.map(bottomRow => createNewDescription(bottomRow))
+
+/* Fill cards */
+
+
+/*Get the ingredients*/
+/*function getIngredient(recipe, index) {
+    console.log(recipe, index)
+    const ulIngredients = document.createElement("ul")
+    for (let j=0; j<recipe.ingredients.length; j++){
+        const newIngredient = document.createElement("li");
+        newIngredient.textContent = recipe.ingredients[j].ingredient
+        ulIngredients.appendChild(newIngredient)
+        /*.appendChild(newIngredient);
+        const recipeIngredient = document.querySelectorAll(".ingredients > li");
+        recipeIngredient[index].innerHTML = recipe.ingredients[j].ingredient;*/
+    //}
+    //document.querySelectorAll(".ingredients")[index].appendChild(ulIngredients)
+    //return ulIngredients;
+//}
+
 /*Fill the cards*/
-function fillCards(){
+/*function fillCards(){
     for(let i=0; i<recipes.length; i++){
         const card = document.querySelector(".card");
         let numberCard = document.querySelectorAll(".card").length
@@ -33,9 +125,8 @@ function fillCards(){
         
         getIngredient(recipes[i], i);
     }
-    
 }
-fillCards()
+fillCards()*/
 
 /*Get lists for buttons*/
 const listUstensils = []
@@ -59,7 +150,6 @@ function getLists(){
         ListApparel.push(capAppliance);
     }   
 }
-
 getLists()
 
 /*Delete duplicates*/
@@ -96,25 +186,30 @@ function displayList(){
 }
 displayList();
 
-/* Tag on click */
-const createTag = document.createElement("div")
-const tag = document.getElementById("tag")
+/*** Tag on click ***/
+
 let liTagIngredients = document.querySelectorAll("#myDropdown_I > ul > li")
 
 function listenClickTag(){
     for(let i=0; i<liTagIngredients.length; i++){
-        liTagIngredients[i].addEventListener("click", function(){
+        liTagIngredients[i].addEventListener("click", function(e){
+            console.log(e)
+            const createTag = document.createElement("div")
+            createTag.textContent = "coucou"
+            createTag.classList.add("bg-primary", "p-1", "m-1")
+            const tag = document.getElementById("tag")
+
             tag.appendChild(createTag)
-            tag.classList.add("d-flex")
-            let tagIngredient = document.querySelector("#tag > div")
-            tagIngredient.innerHTML = liTagIngredients[i].innerHTML;
-            tagIngredient.classList.add("bg-primary", "p-1", "m-1")
+
+            //let tagIngredient = document.querySelector("#tag > div")
+            //tagIngredient.innerHTML = liTagIngredients[i].innerHTML;
+            
         })
     }
 }
 listenClickTag()
 
-/* main search bar */
+/*** Main search bar ***/
 const mainBar = document.getElementById("search_bar")
 
 /* Array match test */
@@ -195,7 +290,7 @@ function displayIngredients(){
             console.log([...new Set(matchArrayIngredients)])
         }else{console.log("no")}
     }
-    console.log([...new Set(matchArrayIngredients)])
+    console.log("yes", [...new Set(matchArrayIngredients)])
     fillList([...new Set(matchArrayIngredients)],0);
 }
 
