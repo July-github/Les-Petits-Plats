79889029a935650015a8e684b881783ae46235fa
Julie Heckler <july.heckler@gmail.com>
2021-12-15 18:24:59 +0100

    more filter

--- js/mix_filters.js
+++ js/mix_filters.js
@@ -61,7 +61,7 @@ function reduceArray(textParentTag, tagMatched){
     }
 }
 
-/** Several different types of tags & Main search bar **/
+/** Several different types of tags **/
 function reduceMixArray(textParentTagX, tagMatchedX, tagMatchedY, textParentTagY){
     const reducedX = reduceArray(textParentTagX, tagMatchedX)
     for (let i=0; i<textParentTagY.length; i++){
@@ -118,8 +118,10 @@ export function testAlreadyTag(){
 
     }if((testTagI !== 0) && (testTagA !== 0) && (testTagU !== 0)){
         const arrayRecipe = reduceMixArray(textParentTagI, tagIMatched, tagAMatched, textParentTagA)
-        return tagUMatched(arrayRecipe, textParentTagU.slice(-1)[0])
-
+        for (let i=0; i<textParentTagU.length; i++){
+            const arrayTagU = tagUMatched(arrayRecipe, textParentTagU[i])
+            return tagUMatched(arrayTagU, textParentTagU.slice(-1)[0])
+        }
     }if((testTagI === 0) && (testTagA === 0) && (testTagU === 0)){
         return recipes
     }
